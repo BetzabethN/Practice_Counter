a200ffb830736e992eacfa65eedaad1d2eccfa59
Betzabeth Narvaez <betzynarv@gmail.com>
2021-10-10 19:53:15 +0900

    add buttons and counting function

--- app/src/main/java/com/example/counter/MainActivity.java
+++ app/src/main/java/com/example/counter/MainActivity.java
@@ -9,10 +9,32 @@ import com.example.counter.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
 
+    private int count = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.buttonPlus.setOnClickListener(v -> plusCount());
+        binding.buttonMinus.setOnClickListener(v -> minusCount());
+    }
+
+    private void plusCount() {
+        count++;
+        updateCount();
+    }
+
+    private void minusCount() {
+        count--;
+        if (count < 0) {
+            count = 0;
+        }
+        updateCount();
+    }
+
+    private void updateCount() {
+        binding.counter.setText(Integer.toString(count));
     }
 }
